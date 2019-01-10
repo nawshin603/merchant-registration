714bcc94be9dd5279a7b4f4939742055ca9b3824
nawshin603 <nawshin603@gmail.com>
2019-01-10 11:23:03 +0600

    different domain

--- frm_merchant_reg.php
+++ frm_merchant_reg.php
@@ -1,13 +1,32 @@
-<div class="stdFormHeader"> New Merchant Registration</div>
-<form action="merchants.php" method="POST">
-    <label class="stdFormLabel">Merchant name : </label><input class="stdFormInput" type="text" name="merchantName" required><br>
-<!--    <label class="stdFormLabel">Business Type : </label><select class="stdFormSelect" name="shopMarket" required>-->
-<!--        <option value="shop">Shop</option>-->
-<!--        <option value="market">Market Place</option>-->
-<!--    </select><br>-->
-    <label class="stdFormLabel">Contact Person : </label><input class="stdFormInput" type="text" name="contactPerson" required><br>
-    <label class="stdFormLabel">Contact Number : </label><input class="stdFormInput" type="text" name="contactNumber" required><br>
-    <label class="stdFormLabel">Address : </label><textarea class="stdFormInputBox" name="address"></textarea><br>
+<script>
+    function myfunction(){
 
-    <input class="stdFormButton"  type="submit" name="submit" value="Apply">
+         $.ajax({
+           type: 'post',
+           url: 'merchants.php',
+           data: $('form').serialize(),
+           success: function () {
+             alert('form was submitted');
+           }
+         });
+          $.ajax({
+           type: 'post',
+           url: 'http://paperflybd.com/api_temporary_merchant_registration.php',
+           data: $('form').serialize(),
+           success: function () {
+             alert('form was submitted');
+           }
+         });
+     }
+
+
+   </script>
+
+<div class="stdFormHeader"> New Merchant Registration</div>
+<form action="" method="POST">
+   <label class="stdFormLabel">Merchant name : </label><input class="stdFormInput" type="text" name="merchantName" required><br>
+   <label class="stdFormLabel">Contact Person : </label><input class="stdFormInput" type="text" name="contactPerson" required><br>
+   <label class="stdFormLabel">Contact Number : </label><input class="stdFormInput" type="text" name="contactNumber" required><br>
+   <label class="stdFormLabel">Address : </label><textarea class="stdFormInputBox" name="address"></textarea><br>
+<input class="stdFormButton"  type="submit" name="submit" onClick="myfunction()" value="Apply">
 </form>
